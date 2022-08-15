28dad1b03d19365aab4842c4e31dcc08eabd6a9f
Your name <abdullah.ibna.aamir@outlook.com>
2022-08-16 07:49:24 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,179 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require("path")
+const { Server } = require('http');
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'base.html';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/one", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Abu Bakr.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/two", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Umar.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/three", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Uthman.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/four", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Ali.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/five", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Talhah.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/six", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Az Zubayr.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/seven", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Abdur Rahman.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/eight", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Saad.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/nine", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Sa id.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+app.get("/ten", function(req, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    var fileName = 'Abu Ubaidah.htm';
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000")
+});
